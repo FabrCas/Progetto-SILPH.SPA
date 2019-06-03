b4232d454c35e1a011470e6faa4ee535781860f8
antonio-ianniello <antonio.ianniello509477@gmail.com>
2019-06-03 15:02:38 +0200

    aggiunti getters  && setters funzionario,galleria,richiestafoto

--- Progetto Silph/src/Model/FunzionarioSilph.java
+++ Progetto Silph/src/Model/FunzionarioSilph.java
@@ -9,5 +9,47 @@ public class FunzionarioSilph {
 	private Galleria gallery;
 	private Map<Long,RichiestaFoto> richieste;
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	//getters && setters
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public Galleria getGallery() {
+		return gallery;
+	}
+	public void setGallery(Galleria gallery) {
+		this.gallery = gallery;
+	}
+	public Map<Long, RichiestaFoto> getRichieste() {
+		return richieste;
+	}
+	public void setRichieste(Map<Long, RichiestaFoto> richieste) {
+		this.richieste = richieste;
+	}
+	
+	
+	
 
 }
