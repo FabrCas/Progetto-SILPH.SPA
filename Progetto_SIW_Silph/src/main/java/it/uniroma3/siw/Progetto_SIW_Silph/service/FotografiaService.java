6e07fb23dc9daba319ecbe9a6136dbdb08adc663
fabrizio <fabriziocasadei97@gmail.com>
2019-06-15 21:53:29 +0200

    Finita l'implementazione iniziale dei servizi

--- Progetto_SIW_Silph/src/main/java/it/uniroma3/siw/Progetto_SIW_Silph/service/FotografiaService.java
+++ Progetto_SIW_Silph/src/main/java/it/uniroma3/siw/Progetto_SIW_Silph/service/FotografiaService.java
@@ -1,8 +1,32 @@
 package it.uniroma3.siw.Progetto_SIW_Silph.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import it.uniroma3.siw.Progetto_SIW_Silph.model.Fotografia;
+import it.uniroma3.siw.Progetto_SIW_Silph.repository.FotografiaRepository;
+
 
 @Service
 public class FotografiaService {
-
+	@Autowired
+	private FotografiaRepository fotografiaRepository;
+	
+	@Transactional
+	public Fotografia Inserisci (Fotografia f) {
+		//questa classe dipende dal repository
+		return fotografiaRepository.save(f);
+	}
+	
+	@Transactional
+	public List<Fotografia> tutteLeFotografie () {
+		return (List<Fotografia>) fotografiaRepository.findAll();
+	}
+	
+	public Fotografia FotografiaPerId (Long id) {
+		return this.fotografiaRepository.findById(id).get();
+	}
 }
--- Progetto_SIW_Silph/src/main/java/it/uniroma3/siw/Progetto_SIW_Silph/service/RichiestaFotoService.java
+++ Progetto_SIW_Silph/src/main/java/it/uniroma3/siw/Progetto_SIW_Silph/service/RichiestaFotoService.java
@@ -1,8 +1,32 @@
 package it.uniroma3.siw.Progetto_SIW_Silph.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import it.uniroma3.siw.Progetto_SIW_Silph.model.RichiestaFoto;
+import it.uniroma3.siw.Progetto_SIW_Silph.repository.RichiestaFotoRepository;
+
 
 @Service
 public class RichiestaFotoService {
-
+	@Autowired
+	private RichiestaFotoRepository richiestaFotoRepository;
+	
+	@Transactional
+	public RichiestaFoto Inserisci (RichiestaFoto r) {
+		//questa classe dipende dal repository
+		return richiestaFotoRepository.save(r);
+	}
+	
+	@Transactional
+	public List<RichiestaFoto> tutteLeRichiesteFoto () {
+		return (List<RichiestaFoto>) richiestaFotoRepository.findAll();
+	}
+	
+	public RichiestaFoto RichiestaFotoPerId (Long id) {
+		return this.richiestaFotoRepository.findById(id).get();
+	}
 }
