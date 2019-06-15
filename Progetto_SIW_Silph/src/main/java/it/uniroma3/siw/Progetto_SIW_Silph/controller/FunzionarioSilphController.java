5dfcf3d06a681d39294bd8b3854682e7036b0552
antonio-ianniello <antonio.ianniello509477@gmail.com>
2019-06-15 17:05:32 +0200

    conto

--- Progetto_SIW_Silph/src/main/java/it/uniroma3/siw/Progetto_SIW_Silph/controller/FunzionarioSilphController.java
+++ Progetto_SIW_Silph/src/main/java/it/uniroma3/siw/Progetto_SIW_Silph/controller/FunzionarioSilphController.java
@@ -2,7 +2,7 @@ package it.uniroma3.siw.Progetto_SIW_Silph.controller;
 
 public class FunzionarioSilphController {
 	
-	private String prova;
+	private int conto;
 	
 	//TODO
 	public String NewFunzionario() {
