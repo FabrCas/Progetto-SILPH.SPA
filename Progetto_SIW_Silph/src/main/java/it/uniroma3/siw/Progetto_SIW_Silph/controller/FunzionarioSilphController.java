150c034a98b28e51456c427bcbddafa899e7746d
fabrizio <fabriziocasadei97@gmail.com>
2019-06-17 11:27:24 +0200

    Inzio della creazione dei templeta html e relativi controller

--- Progetto_SIW_Silph/src/main/java/it/uniroma3/siw/Progetto_SIW_Silph/controller/FunzionarioSilphController.java
+++ Progetto_SIW_Silph/src/main/java/it/uniroma3/siw/Progetto_SIW_Silph/controller/FunzionarioSilphController.java
@@ -1,13 +1,40 @@
 package it.uniroma3.siw.Progetto_SIW_Silph.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import it.uniroma3.siw.Progetto_SIW_Silph.service.RichiestaFotoService;
 
 @Controller
 public class FunzionarioSilphController {
+	@Autowired
+	RichiestaFotoService richiestaFotoService;
 	
-	//TODO
-	public String NewFunzionario() {
-		return null;
+	
+
+	
+	@RequestMapping("/attivitaFunzionario")
+	public String attivitaFunzionario() {
+		return "reindirizzamentoFunzionario.html";
 	}
+	
+	@RequestMapping("/toSito")
+	public String vaiAlSito() {
+		return "paginaSito.html";
+	}
+	
+	@RequestMapping("/inserimentoDati")
+	public String InserimentoNuoviDati() {
+		return "salvataggioNuoviDati.html";
+	}
+	
+	@RequestMapping("/visualizzaRichieste")
+	public String visualizzaRichieste(Model model){
+		model.addAttribute("richieste", this.richiestaFotoService.tutteLeRichiesteFoto());
+		return "richiesteDiUtilizzo.html";
+	}
+	
 	
 	
 
