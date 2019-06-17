1367cd5b59ed0e166374b3e9ee638f6e6ea851a0
fabrizio <fabriziocasadei97@gmail.com>
2019-06-17 15:40:02 +0200

    implementata la scelta del funzionario di scegliere quale dati salvare
    manca l'implementazione dei singoli salvataggi

--- Progetto_SIW_Silph/src/main/java/it/uniroma3/siw/Progetto_SIW_Silph/controller/FunzionarioSilphController.java
+++ Progetto_SIW_Silph/src/main/java/it/uniroma3/siw/Progetto_SIW_Silph/controller/FunzionarioSilphController.java
@@ -1,26 +1,37 @@
 package it.uniroma3.siw.Progetto_SIW_Silph.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class FunzionarioSilphController {
 	
 	
-	
+	//dopo l'accesso, solo funzionario
 	@RequestMapping("/attivitaFunzionario")
 	public String attivitaFunzionario() {
 		return "reindirizzamentoFunzionario.html";
 	}
 	
+	//dall'index, funzionario e utente
 	@RequestMapping("/toSito")
 	public String vaiAlSito() {
 		return "paginaSito.html";
 	}
 	
+	//funzionario
 	@RequestMapping("/inserimentoDati")
 	public String InserimentoNuoviDati() {
 		return "salvataggioNuoviDati.html";
+	}
+	
+	//funzionario
+	@RequestMapping(value="/operazioniFunzionario", method=RequestMethod.POST)
+	public String operazioneGiusta(@RequestParam("operazione") String operazioneValore) {
+		return "/new"+operazioneValore;
+		
 	}
 	
 
