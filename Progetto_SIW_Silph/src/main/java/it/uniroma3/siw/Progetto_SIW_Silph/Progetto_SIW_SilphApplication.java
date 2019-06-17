5ca0c510f1d0771e4eddbc8220ee37fddacf27b6
fabrizio <fabriziocasadei97@gmail.com>
2019-06-17 18:33:35 +0200

    Problema riscontrato nelle form con componenti multipli di altri oggetti

--- Progetto_SIW_Silph/src/main/java/it/uniroma3/siw/Progetto_SIW_Silph/Progetto_SIW_SilphApplication.java
+++ Progetto_SIW_Silph/src/main/java/it/uniroma3/siw/Progetto_SIW_Silph/Progetto_SIW_SilphApplication.java
@@ -3,11 +3,13 @@ package it.uniroma3.siw.Progetto_SIW_Silph;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Progetto_SIW_SilphApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Progetto_SIW_SilphApplication.class, args);
+	
 	}
 
 }
--- Progetto_SIW_Silph/src/main/java/it/uniroma3/siw/Progetto_SIW_Silph/controller/FunzionarioSilphController.java
+++ Progetto_SIW_Silph/src/main/java/it/uniroma3/siw/Progetto_SIW_Silph/controller/FunzionarioSilphController.java
@@ -1,8 +1,8 @@
 package it.uniroma3.siw.Progetto_SIW_Silph.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -26,17 +26,4 @@ public class FunzionarioSilphController {
 	public String InserimentoNuoviDati() {
 		return "salvataggioNuoviDati.html";
 	}
-	
-	//funzionario
-	@RequestMapping(value="/operazioniFunzionario", method=RequestMethod.POST)
-	public String operazioneGiusta(@RequestParam("operazione") String operazioneValore) {
-		return "gestione"+operazioneValore+".html";
-		
-	}
-	
-
-	
-	
-	
-
 }
